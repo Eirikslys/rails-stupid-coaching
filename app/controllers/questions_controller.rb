6ce050b2d524f17ea7f6be34b5b29aadaa6c19e0
eirik lysenstoen <elysenstoen@gmail.com>
2020-08-11 20:16:38 +0200

    the coach will now take your questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,5 +3,13 @@ class QuestionsController < ApplicationController
   end
 
   def answer
+    # raise
+    if params[:answer] == "I am going to work"
+      @answer = "Great!"
+    elsif params[:answer][-1] == "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
   end
 end
