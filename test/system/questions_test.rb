f19621c16c182fbd5337a17f4244465df35deb82
eirik lysenstoen <elysenstoen@gmail.com>
2020-08-13 18:02:10 +0200

    tests work now

--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -11,7 +11,7 @@ class QuestionsTest < ApplicationSystemTestCase
     assert_selector "p", text: "Ask your coach anything"
   end
 
-    test "saying Hello yields a grumpy response from the coach" do
+  test "saying Hello yields a grumpy response from the coach" do
     visit ask_url
     fill_in "question", with: "Hello"
     puts @question
@@ -19,4 +19,23 @@ class QuestionsTest < ApplicationSystemTestCase
 
     assert_text "I don't care, get dressed and go to work!"
   end
+
+  test "asking a question returns I don't care return to work" do
+    visit ask_url
+    fill_in "question", with: "Hello?"
+    puts @question
+    click_on "Ask"
+
+    assert_text "Silly question, get dressed and go to work!"
+  end
+
+  test "Saying you will go to work is answered with great" do
+    visit ask_url
+    fill_in "question", with: "I am going to work"
+    puts @question
+    click_on "Ask"
+
+    assert_text "Great!"
+  end
+
 end
